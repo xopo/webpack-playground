aafcba55982f642e3e301539148b18a0d5f3e23c
Daniel <opodaniel@yahoo.com>
2020-07-03 16:48:32 +0300

    add css loader into js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
 import { run } from './app/app';
+import './main.css';
 
 run(new AlertService(), new ComponentService());
